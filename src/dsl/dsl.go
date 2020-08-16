ee94070ace7e16095f748fb6df7b6c5e36ee0b74
Nate <nathanid95@gmail.com>
2020-08-16 01:09:06 -0700

    updated execute func that uses the new parser

--- src/dsl/dsl.go
+++ src/dsl/dsl.go
@@ -40,73 +40,99 @@ func Execute(input string, conf config.Config) (store.Result, error) {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
 		return autoIndex.Query(query.autoSel)
+
 	case typeQueryKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
 		return mapIndex.Query(query.mapSel)
+
 	case typeInsert:
 		autoIndex, err := store.NewAutoIndex(query.indexName, storageDriver, autoPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
 		return autoIndex.Insert(query.payload)
+
 	case typeInsertKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return mapIndex.Insert(query.mapSel, query.payload)
+
 	case typeUpdate:
 		autoIndex, err := store.NewAutoIndex(query.indexName, storageDriver, autoPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return autoIndex.Update(query.autoSel, query.payload)
+
 	case typeUpdateKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return mapIndex.Update(query.mapSel, query.payload)
+
 	case typeUpsertKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return mapIndex.Upsert(query.mapSel, query.payload)
+
 	case typeDelete:
 		autoIndex, err := store.NewAutoIndex(query.indexName, storageDriver, autoPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return autoIndex.Delete(query.autoSel)
+
 	case typeDeleteKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return mapIndex.Delete(query.mapSel)
+
 	case typeList:
 		autoIndex, err := store.NewAutoIndex(query.indexName, storageDriver, autoPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return autoIndex.List(query.limit, query.offset)
+
 	case typeListKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return mapIndex.List(query.limit, query.offset)
+
 	case typeCount:
 		autoIndex, err := store.NewAutoIndex(query.indexName, storageDriver, autoPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return autoIndex.Count()
+
 	case typeCountKey:
 		mapIndex, err := store.NewMapIndex(query.indexName, storageDriver, mapPageSize)
 		if err != nil {
 			return store.EmptyResult(), fmt.Errorf("reading index %s failed: %w", query.indexName, err)
 		}
+		return mapIndex.Count()
+
 	case typeCreateAutoIndex:
 		return store.SingleResult(query.indexName), storageDriver.CreateAutoIndex(query.indexName)
+
 	case typeCreateMapIndex:
 		return store.SingleResult(query.indexName), storageDriver.CreateMapIndex(query.indexName)
 	}
+
+	return store.EmptyResult(), errors.New("query keyword did not match any commands")
 }
 
 func displayCommandList() {
