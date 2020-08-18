dfd25c8b341fd9198ccc2f0da3855676390c2145
Nate <nathanid95@gmail.com>
2020-08-17 21:27:59 -0700

    tests for basic auto page index

--- src/store/page_test.go
+++ src/store/page_test.go
@@ -159,3 +159,82 @@ func TestPathToIndexPage(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestPageAppendQuery(t *testing.T) {
+	p := store.EmptyPage("test_page")
+	testVal := "the test value"
+
+	id := p.Append(testVal)
+
+	val, err := p.Query(id)
+	util.Ok(t, err)
+	util.Equals(t, testVal, val)
+	util.Equals(t, 1, p.Length())
+}
+
+func TestPageAppendDelete(t *testing.T) {
+	p := store.EmptyPage("test_page")
+	testVal := "the test value to delete"
+
+	id := p.Append(testVal)
+
+	val, err := p.Query(id)
+	util.Ok(t, err)
+	util.Equals(t, testVal, val)
+
+	err = p.Delete(id)
+	util.Ok(t, err)
+
+	util.Equals(t, 0, p.Length())
+
+	val, err = p.Query(id)
+	util.Assert(t, err != nil, "retrieving missing key should return error")
+	util.Equals(t, "", val)
+
+	err = p.Delete(id)
+	util.Assert(t, err != nil, "deleting missing key should return error")
+}
+
+func TestPageOverwrite(t *testing.T) {
+	p := store.EmptyPage("test_page")
+	initial := "initial"
+
+	id := p.Append(initial)
+
+	val, err := p.Query(id)
+	util.Ok(t, err)
+	util.Equals(t, initial, val)
+
+	// test updated value is stored
+	updated := "updated"
+	err = p.Overwrite(id, updated)
+	util.Ok(t, err)
+
+	val2, err := p.Query(id)
+	util.Ok(t, err)
+	util.Equals(t, updated, val2)
+
+	// test updating non-existent value fails
+	err = p.Overwrite(10, "uh-oh!")
+	util.Assert(t, err != nil, "updating unregistered key should throw error")
+}
+
+func TestSetMinimumKey(t *testing.T) {
+	p := store.EmptyPage("test_page")
+	testVal := "initial"
+	var minKey uint64 = 100
+
+	p.SetMinimumKey(minKey)
+	id := p.Append(testVal)
+
+	util.Assert(t, id == minKey, "first insert should be equal to miminum map key")
+
+	val, err := p.Query(id)
+	util.Ok(t, err)
+
+	util.Equals(t, testVal, val)
+
+	// for page of size 1, max key should equal min key
+	maxKey := p.MaxKey()
+	util.Equals(t, maxKey, minKey)
+}
